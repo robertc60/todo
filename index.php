0e59c08ac2ff9819feeccea91139895074463964
Robert Clarkson <robertc60@live.com>
2019-08-06 21:46:58 +0100

    added db function

--- index.php
+++ index.php
@@ -7,13 +7,52 @@
 </head>
 <body>
 <div>
-    <form class="todo">
+    <form class="todo" action="index.php" method="post" id="toDoList">
 
          <label class="todoLabel">
              List Item: <br>
-             <input type="text" class="todoLabel" name="txtTodo">
+             <input type="text" class="todoText" name="txtTodo">
+             <input type="submit" class="todoText" value="Add" name="toDoSubmit">
          </label>
+
     </form>
+
+    <?php
+    //get user input
+    if (isset($_POST['toDoSubmit'])) {
+        //adding input to list if the submit button is pressed
+
+        $listItem = $_POST['txtTodo'];
+    }
+    // create a connection to the db
+    $mysqli = new mysqli("localhost", "root", "lampclock", "todo");
+    //check connection to db
+    if ($mysqli->connect_error){
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+    //build sql insert query
+    $sql = "INSERT INTO toDoList(task) VALUES ('$listItem')";
+    //run sql query
+    if ($mysqli->query($sql) === TRUE){
+        echo "TASK Added!";
+    }
+    else{
+        echo "Error: " . $sql . "<br>" . $mysqli->error;
+    }
+    ?>
+    <ul>
+        <?php
+            //save database values to array
+            $collect = $mysqli->query("SELECT task FROM toDoList");
+            //initialise array
+            $list = array();
+            $list = $mysqli->query($collect);
+
+            foreach ($list as $listItem){
+                echo "<li>$listItem</li>";
+            }
+    ?>
+    </ul>
 </div>
 </body>
 </html>
