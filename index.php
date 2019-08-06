033bd97eb1cee9519fec72379078e3e75c8055dc
Robert Clarkson <robertc60@live.com>
2019-08-06 16:59:30 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Todo List</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div>
+    <form class="todo">
+
+         <label class="todoLabel">
+             List Item: <br>
+             <input type="text" class="todoLabel" name="txtTodo">
+         </label>
+    </form>
+</div>
+</body>
+</html>
