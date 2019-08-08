79be8a5dc4e2aaa89047e1ffd109b39eb2599b32
Robert Clarkson <robertc60@live.com>
2019-08-08 14:55:33 +0100

    refactored code displaying table rows whith a while loop, full CRUD dunctionality implemented

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+    //errors var
+    $errors = "";
+    //connect to db
+    $db = mysqli_connect("localhost", "robert", "Tangoecho44", "todo");
+
+    //insert task to db
+    if (isset($_POST['submit'])){
+        if (empty($_POST['task'])){
+            $errors = "You must fill in the task";
+        }
+        else{
+            $task = $_POST['task'];
+            $sql = "INSERT INTO toDoList(task) VALUES ('$task')";
+            mysqli_query($db, $sql);
+            header('location: index.php');
+        }
+    }
+
+    if (isset($_GET['del_task'])){
+        $id = $_GET['del_task'];
+
+        mysqli_query($db, "DELETE FROM toDoList WHERE id=".$id);
+        header('location: index.php');
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,53 +33,35 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<div>
-    <form class="todo" action="index.php" method="post" id="toDoList">
-
-         <label class="todoLabel">
-             List Item: <br>
-             <input type="text" class="todoText" name="txtTodo">
-             <input type="submit" class="todoText" value="Add" name="toDoSubmit">
-         </label>
-
-    </form>
-
-    <?php
-    //get user input
-    if (isset($_POST['toDoSubmit'])) {
-        //adding input to list if the submit button is pressed
-
-        $listItem = $_POST['txtTodo'];
-    }
-    // create a connection to the db
-    $mysqli = new mysqli("localhost", "robert", "Tangoecho44", "todo");
-    //check connection to db
-    if ($mysqli->connect_error){
-        die("Connection failed: " . $mysqli->connect_error);
-    }
-    //build sql insert query
-    $sql = "INSERT INTO toDoList(task) VALUES ('$listItem')";
-    //run sql query
-    if ($mysqli->query($sql) === TRUE){
-        echo "TASK Added!";
-    }
-    else{
-        echo "Error: " . $sql . "<br>" . $mysqli->error;
-    }
-    ?>
-    <ul>
-        <?php
-            //save database values to array
-            $collect = $mysqli->query("SELECT task FROM toDoList");
-            //initialise array
-            $list = array();
-            $list = $mysqli->query($collect);
-
-            foreach ($list as $listItem){
-                echo "<li>$listItem</li>";
-            }
-    ?>
-    </ul>
+<div class="heading">
+    <h2>Robert's Todo List</h2>
 </div>
+    <form class="input_form" action="index.php" method="post">
+        <?php if (isset($errors)){?>
+        <p><?php echo $errors;?></p>
+        <?php } ?>
+             <input type="text" class="task_input" name="task">
+             <button type="submit" class="add_btn" name="submit" id="add_btn">Add Task</button>
+    </form>
+<table>
+        <tr>
+            <td>N</td>
+            <td>Tasks</td>
+            <td style="width: 60px">Action</td>
+        </tr>
+        <?php
+            //select all tasks
+            $tasks = mysqli_query($db, "SELECT * FROM toDoList");
+            $i = 1; while ($row = mysqli_fetch_array($tasks)){ ?>
+            <tr>
+                <td> <?php echo $i ?></td>
+                <td class="task"> <?php echo $row['task']; ?></td>
+                <td class="delete">
+                    <a href="index.php?del_task=<?php echo $row['id'] ?>">X</a>
+                </td>
+            </tr>
+        <?php $i++;} ?>
+</table>
+
 </body>
 </html>
